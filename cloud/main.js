96e4191ec678b0e5f3d0619534ada0eb02b320c1
MelikovMan <melikvov@mail.ru>
2023-09-28 10:40:26 +0300

    Add files via upload

--- cloud/main.js
+++ cloud/main.js
@@ -48,7 +48,7 @@ window.addEventListener('DOMContentLoaded',(e)=>{
         e.preventDefault();
           let select = document.getElementById("lang-field");
           let b = canvas.toDataURL().replace('data:', '').replace(/^.+,/, '');
-          let l = select.options[select.selectedIndex].value
+          let l = select.options[select.selectedIndex].value;
           let obj = {
               base64 = b,
               language = l
@@ -73,11 +73,11 @@ window.addEventListener('DOMContentLoaded',(e)=>{
                   return response;
               })
               .then((response) => {
-                  alert("Успешная отправка");
+                  alert("Success");
                   response.json()
               }).then((response) => console.log(response))
               .catch((error) => {
-                  alert("Ошибка отправки");
+                  alert("Fail");
                   console.log(error);
               }).finally(() => document
                   .getElementById('submitbutton')
